7a41e699ed08e20cf0dee2377c633fa0022745b4
JIMMY <jimmy@MacBook-Pro-di-JIMMY.local>
2018-04-17 20:12:11 +0200

    Soluzione proposta

--- VotiNobel/src/it/polito/tdp/model/Model.java
+++ VotiNobel/src/it/polito/tdp/model/Model.java
@@ -1,14 +1,84 @@
 package it.polito.tdp.model;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import it.polito.tdp.dao.EsameDAO;
 
 public class Model {
 
+	private List<Esame> esami;
+	private EsameDAO edao;
+	private List<Esame> soluzione;
+	private double bestAvg;
+	
+	public Model() {
+		edao = new EsameDAO();
+		esami = edao.getTuttiEsami();
+//		for (Esame e: esami) {
+//			System.out.println(e);
+//		}
+	}
+	
 	public List<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
+		// inizializzazione
+		soluzione = new ArrayList<Esame>();
+		bestAvg = 0.0;
 		
-		System.out.println("TODO!");
+		int step = 0;
+		List<Esame> parziale = new ArrayList<>();
+		recursive(step, parziale, numeroCrediti);
 		
-		return null;
+		return soluzione;
+	}
+	
+	public int totCrediti(List<Esame> parziale) {
+		int somma = 0;
+		for (Esame e: parziale) {
+			somma += e.getCrediti();
+		}
+		return somma;
+	}
+	
+	public double avg(List<Esame> parziale) {
+		double avg = 0;
+		for (Esame e : parziale) {
+			avg += e.getCrediti() * e.getVoto();
+		}
+		avg /= totCrediti(parziale);
+		return avg;
+	}
+	
+	private void recursive(int step, List<Esame> parziale, int numeroCrediti) {
+		
+		// Debug ??
+		// for (Esame e : parziale) {
+		//	System.out.print(e.getCodins() + " ");
+		// }
+		// System.out.println(" ");
+		
+		
+		// Condizione di terminazione
+		if (totCrediti(parziale) > numeroCrediti) {
+			return;
+		}
+		
+		// Controllo se ho trovato una nuova soluzione migliore
+		if (totCrediti(parziale) == numeroCrediti) {
+			if (avg(parziale) > bestAvg) {
+				soluzione = new ArrayList(parziale);
+				bestAvg = avg(parziale);
+			}
+		}
+		
+		// Generazione di una nuova soluzione parziale
+		for (Esame esame : esami) {
+			if (!parziale.contains(esame)) {
+				parziale.add(esame);
+				recursive(step+1, parziale, numeroCrediti);
+				parziale.remove(esame);
+			}
+		}
 	}
 
 }
--- VotiNobel/src/it/polito/tdp/model/TestModel.java
+++ VotiNobel/src/it/polito/tdp/model/TestModel.java
@@ -1,13 +1,19 @@
 package it.polito.tdp.model;
 
+import java.util.List;
+
 public class TestModel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Model model = new Model();
-		model.calcolaSottoinsiemeEsami(20);
+		List<Esame> soluzione = model.calcolaSottoinsiemeEsami(22);
 		
+		System.out.println("Migliore combinazione di esami: ");
+		for (Esame e : soluzione) {
+			System.out.println(e);
+		}
 	}
 
 }
